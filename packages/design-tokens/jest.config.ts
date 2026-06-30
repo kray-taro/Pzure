d9a71d20ce8642d9a7c2c6101da882382216473f
parikimimi <parikimimi@gmail.com>
2026-06-30 10:54:12 +0300

    Revert "Revert "fix(phase-1): complete #69 audit-schema reconciliation + finish Vitest removal""
    This reverts commit 93fc6e568b0dcf432e25dd1ad5543314e8b54528.

---
+++ packages/design-tokens/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+    displayName: 'design-tokens',
+    preset: '../../jest.preset.js',
+    rootDir: '.',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+  };
+  
